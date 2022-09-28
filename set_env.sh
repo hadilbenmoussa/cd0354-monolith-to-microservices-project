cf4a8113bb8906dc1e1afb31f986c76715938d10
hedyyl <91826019+hedyyl@users.noreply.github.com>
2022-09-28 02:02:20 +0200

    refactoring monolithique in microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,9 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=awsudagram
+export POSTGRES_PASSWORD=ha26825066ha
+export POSTGRES_HOST=database-1.cbjj02qmyeuu.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=data-udacity-project
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
