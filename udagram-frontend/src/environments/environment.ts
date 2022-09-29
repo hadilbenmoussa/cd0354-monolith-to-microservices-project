92016d817d333027df7999314f5b0a2f06b513f7
hedyyl <91826019+hedyyl@users.noreply.github.com>
2022-09-29 20:15:08 +0200

    update frontend with reverse ^proxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5b4527fa954f4450abf07b5f0007bf6-380789168.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
